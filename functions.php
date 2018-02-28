12d4fb8abfe7fe5213a17781fa153ecf4459cc43
IndySpaan <indyspaan2@gmail.com>
2018-02-28 22:33:48 +0100

    Fixed jquery bootstrap import

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
 function everydaySetup() {
     wp_enqueue_style( 'bootstrap.min', get_template_directory_uri() . '/assets/css/bootstrap/bootstrap.min.css','1.1','all');
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css','1.1','all');
-    wp_enqueue_script( 'bootstrap.min', get_template_directory_uri() . 'assets/js/bootstrap/bootstrap.min.js', array ( 'jquery' ), 1.1, true);
+    wp_enqueue_script( 'bootstrapjs.min', get_template_directory_uri() . '/assets/js/bootstrap/bootstrap.min.js', array ( 'jquery' ), 1.1);
 
 }
 
