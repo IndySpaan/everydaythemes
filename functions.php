c682e5108851a52584525fac9f7a00372b89c6cd
IndySpaan <indyspaan2@gmail.com>
2018-02-28 22:26:20 +0100

    Fixed basic bootstrap loading (bootstrap.min.js and bootstyrap.min.js) also added basic page (needs to be split up)

--- functions.php
+++ functions.php
@@ -1,2 +1,14 @@
 <?php
 // Silence is golden.
+
+
+function everydaySetup() {
+    wp_enqueue_style( 'bootstrap.min', get_template_directory_uri() . '/assets/css/bootstrap/bootstrap.min.css','1.1','all');
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css','1.1','all');
+    wp_enqueue_script( 'bootstrap.min', get_template_directory_uri() . 'assets/js/bootstrap/bootstrap.min.js', array ( 'jquery' ), 1.1, true);
+
+}
+
+
+add_action( 'wp_enqueue_scripts', 'everydaySetup' );
+
